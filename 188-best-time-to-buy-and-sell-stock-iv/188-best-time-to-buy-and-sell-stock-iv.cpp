57366522d178a04f1d0cff6d19eaccfb8d163d23
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-09-10 23:46:10 +0530

    Time: 15 ms (66.56%), Space: 10.9 MB (84.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& p) {
-        vector<vector<vector<int>>>dp(p.size()+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        // vector<vector<vector<int>>>dp(p.size()+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        vector<vector<int>>cur(2,vector<int>(k+1,0));
+        vector<vector<int>>next(2,vector<int>(k+1,0));
         int pick=0,notpick=0;
         for(int i=p.size()-1;i>=0;i--)
         {
@@ -11,17 +13,18 @@ public:
                 {
                     if(b)
                     {
-                        pick=-p[i]+dp[i+1][0][k1];
-                        notpick=dp[i+1][1][k1];
+                        pick=-p[i]+next[0][k1];
+                        notpick=next[1][k1];
                     }
                     else{
-                        pick=p[i]+dp[i+1][1][k1-1];
-                        notpick=dp[i+1][0][k1];
+                        pick=p[i]+next[1][k1-1];
+                        notpick=next[0][k1];
                     }
-                    dp[i][b][k1]=max(pick,notpick);
+                    cur[b][k1]=max(pick,notpick);
                 }
             }
+            next=cur;
         }
-        return dp[0][1][k];
+        return cur[1][k];
     }
 };
