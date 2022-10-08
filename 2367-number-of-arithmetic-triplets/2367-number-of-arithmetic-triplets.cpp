e82624479d70059ec59b5cf4e1fc5204bbfe9feb
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-08 15:53:45 +0530

    Time: 19 ms (35.16%), Space: 8.7 MB (59.53%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -8,12 +8,16 @@ public:
             // if(i==0||(i>0&&nums[i]!=nums[i-1])){
             for(int j=i+1;j<n;j++)
             {
+                if(nums[j]-nums[i]==diff)//little optimized if first condition satisfied then only it checks for other
+                    
+            {
                 for(int k=j+1;k<n;k++)
                 {
-                    if((nums[j]-nums[i]==diff)&&(nums[k]-nums[j]==diff)){
+                    if((nums[k]-nums[j]==diff)){
                         count++;
                        // cout<<nums[i]<<" "<<nums[j]<<" "<<nums[k]<<endl;
                     }
+                }
                 }
             }
             //}
