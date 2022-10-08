0763d1408f84bdf240756ac249c875b61824a825
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-08 13:11:48 +0530

    Time: 254 ms (28.65%), Space: 23.5 MB (32.61%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>s;
+        for(int i=0;i<n-2;i++)
+        {
+            if(i==0||i>0&&nums[i]!=nums[i-1]){
+            int low=i+1,high=n-1;
+            int target=-nums[i];
+            
+            while(low<high)
+            {
+                vector<int>v;
+                int sum=nums[low]+nums[high];
+                if(sum==target)
+                {
+                  //  cout<<"true";
+                    v.push_back(nums[low]);
+                    v.push_back(nums[high]);
+                    v.push_back(nums[i]);
+                    sort(v.begin(),v.end());
+                    s.push_back(v);
+                    
+                    
+                while(low<high&&nums[low]==nums[low+1])low++;
+                while(low<high && nums[high]==nums[high-1])high--;
+                low++;
+                high--;
+                }
+               
+                else if(sum<target)
+                    low++;
+                else
+                    high--;
+            }
+        }
+        }
+        //vector<vector<int>>v1=s;
+        return s;
+    }
+};
