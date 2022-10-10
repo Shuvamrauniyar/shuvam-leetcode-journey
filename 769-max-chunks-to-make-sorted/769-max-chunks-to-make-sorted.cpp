7f9567611d828a8137de1c5c134de732090cd640
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-10 22:28:55 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (27.88%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -4,16 +4,20 @@ public:
         int n=a.size();
         int count=0;
         int maxi=0;
-       // if(a.size()==1)return 1;
         for(int i=0;i<n;i++)
         {
-            if(a[i]>maxi){
+            if(a[i]>maxi)
                 maxi=a[i];
-            }
-                if(i==maxi)
+                if(maxi==i)
                   count++;
-              
+               // cout<<maxi<<endl;
+            
+            // else if(count>0);
+            // else
+            //     count=0;
         }
+       // if(a[n-1]<a[])
+        //count++;
         return count;
     }
 };
