8120d7f832fa81c0a24b3485e67a13affe359792
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-11 21:30:25 +0530

    Time: 43 ms (69.66%), Space: 17.5 MB (77.87%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.rbegin(),g.rend());
-        sort(s.rbegin(),s.rend());
+        sort(s.rbegin(),s.rend()); //sorting both vectors in descending order
         int count=0;
         for(int i=0,j=0;i<g.size()&&j<s.size();)
         {
-            if(g[i]<=s[j]){
+            if(g[i]<=s[j])
+            {  
                 count++;
-                i++,j++;
+                i++,j++;  //if condition satisfied ,we will increase both the pointers
             }
             else
-                i++;
+                i++; //if not satisfied we will try to find the children by increasing 'i' which can be                              satisfied with current s[j]
             
         }
     
