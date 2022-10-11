f4e0116610af5f877e3d0b8c52e00b673a9f7a54
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-11 15:37:06 +0530

    Time: 42 ms (86.28%), Space: 13.6 MB (17.26%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    //using hashmap in O(n^2)-time and space -O(n^2)
     int countQuadruplets(vector<int>& nums) {
         int n=nums.size();
         int count=0;
-        for(int i=0;i<n;i++)
+        unordered_map<int,int>m;
+        for(int i=1;i<n-2;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=0;j<i;j++)
             {
-                for(int k=j+1;k<n;k++)
-                {
-                    for(int l=k+1;l<n;l++)
-                        if(nums[i]+nums[j]+nums[k]==nums[l])
-                            count++;
-                }
+                m[nums[i]+nums[j]]++;
             }
+            for(int k=i+2;k<n;k++)
+                {
+                 
+                        count+=m[nums[k]-nums[i+1]];
+                }
         }
         return count;
     }
