beaec6f5aed5b5caede6af77e5f35ed4f82cf37f
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-12 10:48:16 +0530

    Time: 122 ms (5.51%), Space: 21.8 MB (96.06%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -7,21 +7,8 @@ public:
         for(int i=0;i<nums.size()-2;i++)
     {
          sum=nums[i]+nums[i+1]+nums[i+2];
-        double s=(double)sum/2;
-        int j=i;
-            int maxj=i+2;
-            bool check=true;
-       while(j<=maxj)
-       {
-        // cout<<nums[j]<<" ";   
-           if(nums[j++]>=s){
-               check=false;
-               break;
-           }
-       }
-            // cout<<endl;
-           if(check==true)
-            return sum;
+            if(nums[i]<nums[i+1]+nums[i+2])
+              return sum;
     }
     return 0;
     }
