2a52bd95b1be4b12f3e3cbaaf857b243d16f42c2
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-21 20:17:04 +0530

    Time: 141 ms (56.84%), Space: 48.4 MB (77.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             if(i>maxjump) //maxjump demotes that till which index we can go to and i
-                         // 'i' is greater than max jump means we canno jump to 'i' 
+                         // 'i' is greater than max jump means we cannot jump to 'i' 
                 return false;
             maxjump=max(nums[i]+i,maxjump);
         }
