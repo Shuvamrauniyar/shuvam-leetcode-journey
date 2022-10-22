44d11ca4518521d4ce97bcb865699a081c71203a
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-22 09:50:38 +0530

    Time: 2 ms (43.36%), Space: 6.1 MB (31.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int findways(int i,vector<int>&dp)
     {
-           if(i==0)
+           if(i==1)
             return 1;
-        if(i<0)
+        if(i<=0)
             return 0;
         if(dp[i]!=-1)
             return dp[i];
@@ -13,7 +13,7 @@ public:
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        dp[0]=1,dp[1]=1;
+        dp[0]=1,dp[1]=1; //you can also use prev curr technique to optimise space by not declaring dp vector
         for(int i=2;i<=n;i++)
         {
             dp[i]=dp[i-1]+dp[i-2];
