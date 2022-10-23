8d5c0dd094be1e578adb6de6ad923af8e453bcde
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-24 01:16:01 +0530

    Time: 42 ms (84.50%), Space: 21.5 MB (66.74%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
+             sort(nums.begin(),nums.end());
         int d;
         int sum=0;
         for(int i=0;i<n-1;i++)
@@ -19,8 +19,38 @@ public:
         int actualsum=(n*(n+1))/2;
         int missing=actualsum-sum+d;
         return {d,missing};
+       
         
         
         
     }
 };
+/*
+        int n=nums.size();
+   
+         int slow=nums[0];
+        int fast=nums[slow];
+       
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }
+         fast=0;
+        while(fast!=slow)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        int d=nums[slow];
+         int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];   
+        }
+        //sum+=nums[n-1];
+        int actualsum=(n*(n+1))/2;
+        int missing=actualsum-sum+d;
+        return {d,missing};
+*/
