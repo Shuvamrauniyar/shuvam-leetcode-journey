168704b02555720e20e46a0f100e05d49b4598cb
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-27 07:42:33 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    void dfs(int x,int vis[],int V,vector<int>adj[],vector<int>&ans)
+    {
+                  vis[x]=1;
+                  ans.push_back(x);
+          
+              for(int i=0;i<adj[x].size();i++)
+              {
+                  if(vis[adj[x][i]]!=1)
+                {
+                 dfs(adj[x][i],vis,V,adj,ans);
+              }
+          }
+          
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>ans;
+        int vis[V]={0};
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            dfs(i,vis,V,adj,ans);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
