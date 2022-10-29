2defded592506bc53c7b5b47175a674f71fa1b55
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-10-29 23:02:38 +0530

    Time: 24 ms (69.72%), Space: 12 MB (49.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int find(int i,int target,vector<int>&nums)
+    int find(int i,int target,vector<int>&nums,vector<vector<int>>&dp)
     {
         if(i==0)
         {
@@ -9,11 +9,13 @@ public:
             if(target==0||target==nums[i])return 1;
             return 0;
         }
+        if(dp[i][target]!=-1)
+            return dp[i][target];
         int pick=0;
         if(nums[i]<=target)
-         pick=find(i-1,target-nums[i],nums);
-        int notpick=find(i-1,target,nums);
-        return pick+notpick;
+         pick=find(i-1,target-nums[i],nums,dp);
+        int notpick=find(i-1,target,nums,dp);
+        return dp[i][target]=pick+notpick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
@@ -22,7 +24,7 @@ public:
             s+=i;
         if(s<target||(s-target)%2!=0)return 0;
         target=(s-target)/2;
-        
-        return find(n-1,target,nums); 
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return find(n-1,target,nums,dp); 
     }
 };
