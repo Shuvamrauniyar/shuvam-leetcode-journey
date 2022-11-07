d17768e2a7e62065ab23af766359b0eca745c29e
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-11-08 01:31:00 +0530

    Time: 2 ms (45.33%), Space: 6.1 MB (17.80%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s=to_string(num);
-        for(int i=0;i<s.size();i++)
+        //string s=to_string(num);
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     if(s[i]=='6'){
+        //         s[i]='9';
+        //     break;
+        //     }
+        // }
+        // num=stoi(s);
+        
+        int n=num;
+        int last6=-1,pos=-1;
+        
+        while(n)
         {
-            if(s[i]=='6'){
-                s[i]='9';
-            break;
-            }
+            pos++;
+            if(n%10==6)
+                last6=pos;
+            n=n/10;
         }
-        num=stoi(s);
+        num=num+3*pow(10,last6);
         return num;
     }
 };
