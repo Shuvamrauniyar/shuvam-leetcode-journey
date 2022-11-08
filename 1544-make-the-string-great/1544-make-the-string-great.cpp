d5cee3bc80a40c6bc5603fffc577be8dcd1654f1
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-11-08 23:46:08 +0530

    Time: 6 ms (48.12%), Space: 6.4 MB (65.67%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char>st;
+         stack<char>st;
         
         for(int i=s.size()-1;i>=0;i--)
         {
@@ -17,6 +17,17 @@ public:
            st.pop();
         
         }
+        // string ans="";
+        // for(int i=0;i<s.size()-1;i++) //wrong for testcase abBAcC
+        // {
+        //     if(abs(s[i]-s[i+1]!=32))
+        //         ans+=s[i];
+        //     else
+        //         i++;
+        // }
+        // if(i!=s.size())
+            
+       // return ans;
         return s;
     }
 };
