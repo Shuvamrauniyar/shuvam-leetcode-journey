29548244fe1c8e5da0b18d281924b7f0c1ec2647
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-11-08 23:16:18 +0530

    Time: 10 ms (97.02%), Space: 10.5 MB (29.06%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -12,37 +12,37 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         ListNode *slowodd=head;
-        if(head==NULL)
+        if(head==NULL) //if list is empty
             return head;
-        ListNode *fasteven=head->next,*evenhead=head->next;
-            if(slowodd->next==NULL||fasteven->next==NULL)
-                return head;
-        while(fasteven->next&&fasteven->next->next)
+         ListNode *fasteven=head->next,*evenhead=head->next; //this line must be declared after above ;lines bcz if head= null then we cant access head->next;  it will give RE error
+        //     if(slowodd->next==NULL||fasteven->next==NULL) //if list has one or two elements
+        //         return head;
+        while(fasteven&&fasteven->next)
         {
-            slowodd->next=slowodd->next->next;
+            slowodd->next=slowodd->next->next;//assigning alternate node(odd indices)
             fasteven->next=fasteven->next->next;
             slowodd=slowodd->next;
             fasteven=fasteven->next;
-            cout<<fasteven->val<<endl;
+            //cout<<fasteven->val<<endl;
         }
         // cout<<fasteven->val<<endl;
         // fasteven->next=NULL;
-        if(slowodd->next->next)
-        {slowodd->next=slowodd->next->next;
-        slowodd=slowodd->next;
-        }
-         cout<<fasteven->val<<endl;
-        fasteven->next=NULL;
+        // if(slowodd->next->next) //for testcase like [1,2,3,4]
+        // {slowodd->next=slowodd->next->next;
+        // slowodd=slowodd->next;
+        // }
+         //cout<<fasteven->val<<endl;
+       // fasteven->next=NULL; //[2->4->5] here 4 still points 5 but 4 is last element so it must point to null
         slowodd->next=evenhead;
         // cout<<slowodd->next->val<<endl;
-         cout<<head->val<<endl;
-        ListNode *temp=head;
-        while(temp)
-        {
-            cout<<temp->val<<" ";
-            temp=temp->next;
+//          cout<<head->val<<endl;
+//         ListNode *temp=head;
+// //         while(temp)
+//         {
+//             cout<<temp->val<<" ";
+//             temp=temp->next;
             
-        }
+//         }
       return head;
     }
 
