e3ad822036e166f2fd5f542ec9f7b7e751762a7b
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-11-10 14:28:31 +0530

    Time: 21 ms (89.69%), Space: 9.9 MB (93.61%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    string removeDuplicates(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++)
-        {
-            if(st.empty()||st.top()!=s[i])
-                st.push(s[i]);
-            else
-                st.pop();
-        }
-        s="";
-        while(!st.empty())
-        {
-            s+=st.top();
-        st.pop();
-        }
-        reverse(s.begin(),s.end());
-        return s;
+    string removeDuplicates(string& s) {
+        int i = 0;
+        for(int j = 1; j < s.size(); ++j)
+            if (i<0 || s[i] != s[j]) //this condition is same as if(st.empty)||st.top()!=s[j])
+                s[++i] = s[j]; //push means increment i
+            else --i; //pop decrement i
+        cout<<i<<endl;
+        return s.substr(0, i+1 );//i is not inclucive so we go till i+1 to make s[i] come in range in substring
     }
 };
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         stack<char>st;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(st.empty()||st.top()!=s[i])
+//                 st.push(s[i]);
+//             else
+//                 st.pop();
+//         }
+//         s="";
+//         while(!st.empty())
+//         {
+//             s+=st.top();
+//         st.pop();
+//         }
+//         reverse(s.begin(),s.end());
+//         return s;
+//     }
+        
+// };
