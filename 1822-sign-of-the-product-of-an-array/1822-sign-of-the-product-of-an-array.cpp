8ada46d60afac27a605f6ece2136af593b42820a
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-11-17 21:29:27 +0530

    Time: 10 ms (67.52%), Space: 10.2 MB (44.84%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int sign;
+        int negsigncnt=0;
         long long int p=1;
         for(int i=0;i<nums.size();i++)
         {
-            p*=nums[i];
-            if(p<0)
-                sign=-1;
-            else if(p>0)
-                sign=1;
-            else
-            {
+            // p*=nums[i];
+            // if(p<0)
+            //     sign=-1;
+            // else if(p>0)
+            //     sign=1;
+            // else
+            // {
+            //     return 0;
+            // }
+            // p/=abs(nums[i]);// to prevent the signed integer overflow dividing it by same number and to preserve the sign of real product ,using absolute 
+            if(nums[i]<0)
+                negsigncnt++;
+            else if(nums[i]==0)
                 return 0;
-            }
-            p/=abs(nums[i]);
+            
             
         }
-        return sign;
+        cout<<negsigncnt<<endl;
+        if((negsigncnt&1)==0)
+            return 1;
+        return -1;
+        //return sign;
     }
 };
