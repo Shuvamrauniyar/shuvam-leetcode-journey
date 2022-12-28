05915bac60e24651e747a8c8456a8e04e1ecb04a
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2022-12-28 22:57:55 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -5,6 +5,12 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    
+    //here subset is covers subsequences,see testcase [5,2,1], we have 5+1=6 in the ans
+    //in this why only subsequemce approach pick ,not pick technique work bcz we have to find sum
+    // and [1,2,3] or [3,2,1] have same sum thats why we dont bother to find subset as [3,2,1]
+    //but if we are told to print all the subset then another approach should be applied ,that you 
+    //can see in subset II problem
     public:
     void findsubsetsums(int ind,vector<int>&arr,vector<int>&ds,int sum)
     {
